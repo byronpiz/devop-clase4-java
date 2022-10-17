bb2a5efdba6b76d619f6159c08de81fc7c82b69b
no name <62671534+byronpiz@users.noreply.github.com>
2022-10-17 14:46:15 +0000

    se cran test case

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -6,8 +6,23 @@ package com.mycompany.app;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static  void main( String[] args )
     {
         System.out.println( "Hello byron!" );
     }
+  public static int suma(int a,int b){
+    return a+b;
+  }
+   public static String triangulo(int a,int b,int c){
+     if(a+b>c&&b+c>a&&a+c>b){   
+      if(c==b&&b==a){
+        return "Triangulo Equilatero";
+      }
+        if(c==b||b==a||c==a){
+        return "Triangulo Isosceles";
+      }
+     }
+    return "Esto no es un triagulo";
+     
+  }
 }
--- my-app/src/test/java/com/mycompany/app/AppTest.java
+++ my-app/src/test/java/com/mycompany/app/AppTest.java
@@ -1,9 +1,10 @@
 package com.mycompany.app;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
-
+import com.mycompany.app.App;
 /**
  * Unit test for simple App.
  */
@@ -13,8 +14,23 @@ public class AppTest
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
+    public void shouldAnswersuma()
     {
-        assertTrue( true );
+
+    
+        assertTrue(  App.suma(1,2)==3 );
+ 
     }
+  @Test
+    public void shouldAnswer(){
+              assertTrue( true );
+
+    }
+    @Test
+    public void shouldTriangulo(){
+              assertEquals("Esto no es un triagulo", App.triangulo(0,0,0) );
+              assertEquals("Triangulo Isosceles", App.triangulo(2,2,3) );
+              assertEquals("Triangulo Equilatero", App.triangulo(1,1,1) );
+    }
+   
 }
